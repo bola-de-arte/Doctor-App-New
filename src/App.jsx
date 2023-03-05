aedee6f7d02d02da69a07b2d55b1455f360c5392
Bolaji Ajao <bjoo29777@gmail.com>
2023-03-05 17:24:58 -0600

    Second commit. Added a home page to mavigate

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import "./App.scss";
+// react router dom allows me to create navigation in my app
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <>
-      <h1>Yassss</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+export default function Home() {
+  //Needed to export so I can later import elsewhere
+  return <div>Home</div>;
+}
